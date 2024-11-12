b83c4bcecddc088e2b91e94cc2722e300fb8609a
Millo <23leeuw@gmail.com>
2024-11-12 15:33:01 +0100

    asdf

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import MainCanvas from './components/MainCanvas.vue'
 
 </script>
 
 <template>
   <HelloWorld/>
+  <MainCanvas/>
 </template>
---
+++ src/components/MainCanvas.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { ref, onMounted } from 'vue';
+   
+    const canvasRef = ref(null);
+
+    onMounted(() => {
+        const canvas = canvasRef.value;
+        console.log(canvasRef)
+        console.log(canvas);
+        // const ctx = canvas.getContext('2d');
+        // ctx.fillStyle = 'red';
+        // ctx.fillRect(10, 10, 150, 100);
+    });
+
+</script>
+
+<template>
+    <div>
+        <canvas :ref="canvasRef"></canvas>
+    </div>
+</template>
