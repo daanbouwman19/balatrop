b535bcd0496643ec03b0bc6c69f1cb448609c4db
joost Bouman <joost.bouman.nl@gmail.com>
2024-11-14 09:05:54 +0100

    changed up the game

--- src/game.js
+++ src/game.js
@@ -2,8 +2,12 @@ export default class Game {
 
     constructor() {
         this.totalPoints = 0;
+
         this.currentScore = 0;
         this.currentMulti = 0;
+
+        this.duitseFranken = 0;
+        
     }
 
 }
