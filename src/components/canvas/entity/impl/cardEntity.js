ebe9b7057e42dcef0d32cb6a62978da7fd2b0af9
Daan Bouwman <daanbouwman19@gmail.com>
2024-11-14 12:57:10 +0100

    correct card size

--- src/components/canvas/entity/impl/cardEntity.js
+++ src/components/canvas/entity/impl/cardEntity.js
@@ -10,30 +10,31 @@ export class CardEntity extends Entity {
         this.color = color;
     }
 
-    draw(screen, t) {
-        // screen.drawRectangle(this.x, this.y, this.width, this.height, this.color);
+        draw(screen, t) {
+            // screen.drawRectangle(this.x, this.y, this.width, this.height, this.color);
 
-        screen.c().save();
+            screen.c().save();
 
-        let x = screen.mouse.x;
-        let y = screen.mouse.y;
-        let width = 100;
-        let height = 100;
+            let x = screen.mouse.x;
+            let y = screen.mouse.y;
+            let width = 63 * 3;
+            let height = 88 * 3;
 
-        const s = Math.sin(t * 0.01)
-        const color = s > 0 ? 'red' : 'blue';
-        screen.c().translate(x, y);
-        // screen.c().scale(s, 1);
-        screen.c().rotate(0, t * 0.01, 0);
-        screen.c().translate(-width/2, -height/2);
+            const s = Math.sin(t * 0.01)
+            const color = s > 0 ? 'red' : 'blue';
+            screen.c().translate(x, y);
+            
+            screen.c().scale(s, 1);
+            screen.c().rotate(0, t * 0.01, 0);
+            screen.c().translate(-width/2, -height/2);
 
-        screen.drawRectangle(0, 0, width, height, color);
+            screen.drawRectangle(0, 0, width, height, color);
 
-        screen.c().fillText('Hello World', 50, 40);
+            screen.c().fillText('Hello World', 50, 40);
 
-        screen.c().restore();
+            screen.c().restore();
 
-    }
+        }
 
     update(t) {
     }
